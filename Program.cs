e7a3462f6eb6994a9508ea9a372ed2b9aa16a377
Jerar_Depardie <taga4car@gmail.com>
2023-11-28 14:45:06 +0300

    new project Version_1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void FilterStrings(string[] original, out string[] result)
+    {
+        result = new string[original.Length];
+        int resultSize = 0;
+
+        foreach (var str in original)
+        {
+            if (str.Length <= 3)
+            {
+                result[resultSize++] = str;
+            }
+        }
+
+        Array.Resize(ref result, resultSize);
+    }
+
+    static void Main()
+    {
+        // Ввод строки
+        Console.Write("Введите строки через пробелы: ");
+        string inputString = Console.ReadLine();
+
+        // Разделение строки на массив строк
+        string[] inputStrings = inputString.Split(' ');
+
+        // Выходной массив строк
+        string[] outputStrings;
+
+        // Фильтрация строк
+        FilterStrings(inputStrings, out outputStrings);
+
+        // Вывод результата
+        Console.WriteLine("Результат:");
+        foreach (var str in outputStrings)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
